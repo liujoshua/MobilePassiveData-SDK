42f010671b4f43d69688e417bfc64bcba4e43348
Shannon Young <shannon.young@sagebase.org>
2021-01-29 10:16:02 -0800

    Add missing PermissionsConfiguration

--- Sources/MobilePassiveData/Permissions/PermissionConfiguration.swift
+++ Sources/MobilePassiveData/Permissions/PermissionConfiguration.swift
@@ -1,6 +1,6 @@
 //
-//  AsyncActionConfiguration.swift
-//  
+//  PermissionsConfiguration.swift
+//
 //
 //  Copyright © 2017-2021 Sage Bionetworks. All rights reserved.
 //
@@ -33,28 +33,10 @@
 
 import Foundation
 
-/// `AsyncActionConfiguration` defines general configuration for an asynchronous background action
-/// that should be run in the background. Depending upon the parameters and how the action is set
-/// up, this could be something that is run continuously or else is paused or reset based on a
-/// timeout interval.
-///
-/// The configuration is intended to be a serializable object and does not call services, record
-/// data, or anything else.
-///
-/// - seealso: `AsyncActionController`.
-///
-public protocol AsyncActionConfiguration : PermissionsConfiguration {
+/// A model object that defines a configuration for permissions to be requested either as a part of
+/// a step or an async action.
+public protocol PermissionsConfiguration {
     
-    /// A short string that uniquely identifies the asynchronous action within the task.
-    var identifier : String { get }
-    
-    /// The type of the async action.
-    var typeName : String { get }
-    
-    /// An identifier marking the step at which to start the action. If `nil`, then the action will
-    /// be started when the task is started.
-    var startStepIdentifier: String? { get }
-    
-    /// Validate the async action to check for any configuration that should throw an error.
-    func validate() throws
+    /// List of the permissions required for this action.
+    var permissionTypes: [PermissionType] { get }
 }
