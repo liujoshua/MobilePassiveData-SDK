8ba23276cb075803093de6b73a234a0c30c99abf
Joshua Liu <liujoshua@users.noreply.github.com>
2021-10-04 07:11:29 -0700

    weather works

--- passiveData/src/commonMain/kotlin/org/sagebionetworks/assessmentmodel/passivedata/recorder/weather/WeatherResult.kt
+++ passiveData/src/commonMain/kotlin/org/sagebionetworks/assessmentmodel/passivedata/recorder/weather/WeatherResult.kt
@@ -2,10 +2,12 @@ package org.sagebionetworks.assessmentmodel.passivedata.recorder.weather
 
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.sagebionetworks.assessmentmodel.passivedata.ResultData
 
 @Serializable
+@SerialName("weather")
 data class WeatherResult(
     override val identifier: String,
     override val startDate: Instant = Clock.System.now(),
