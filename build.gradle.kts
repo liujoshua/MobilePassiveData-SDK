d5b13ed38e497796623dca638a9b0f74c97cf471
Joshua Liu <liujoshua@users.noreply.github.com>
2021-10-06 08:09:57 -0700

    gralde

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
-        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
     }
 }
