77139a003a55a40b3e6d05713fc2b443c0ea42fa
Joshua Liu <liujoshua@users.noreply.github.com>
2021-10-06 08:35:08 -0700

    iOS fix

--- passiveData/src/commonTest/kotlin/org/sagebionetworks/assessmentmodel/passivedata/recorder/weather/WeatherResultTest.kt
+++ passiveData/src/commonTest/kotlin/org/sagebionetworks/assessmentmodel/passivedata/recorder/weather/WeatherResultTest.kt
@@ -1,22 +1,26 @@
 package org.sagebionetworks.assessmentmodel.passivedata.recorder.weather
 
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.plus
+import org.sagebionetworks.assessmentmodel.passivedata.ResultData
 import org.sagebionetworks.assessmentmodel.passivedata.resultDataSerializersModule
 import kotlin.test.Test
 
 class WeatherResultTest {
+    @Serializable
+    class ResultDataWrapper(val resultData:ResultData)
     @Test
     fun testPolymorphicSerialization() {
 
-        val weatherResult: org.sagebionetworks.assessmentmodel.passivedata.ResultData =
+        val weatherResult: ResultData =
             WeatherResult("identifier", weather = null, airQuality = null)
 
         val jsonCoder = Json {
             serializersModule += resultDataSerializersModule
         }
-        val json = jsonCoder.encodeToString(weatherResult)
+        val json = jsonCoder.encodeToString(ResultDataWrapper(weatherResult))
         println(json)
     }
 }
